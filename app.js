4c12f39f60f5cb371f5904f1ac045df7e6b9d9db
Samir <122364498+Stileexx@users.noreply.github.com>
2024-03-04 20:53:48 +0100

    created html

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const http = require("http");
 const port = 3000;
 
-const server = http.createServer(function (req, res) {});
+const server = http.createServer(function (req, res) {
+  res.write("Hello Node");
+  res.end();
+});
 
 server.listen(port, function (error) {
   if (error) {
