ed989fc3ae0afa632c214cc83c2dad9be822a9ee
Samir <122364498+Stileexx@users.noreply.github.com>
2024-03-04 20:48:35 +0100

    listen server

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-const http = require;
+const http = require("http");
+const port = 3000;
+
+const server = http.createServer(function (req, res) {});
+
+server.listen(port, function (error) {
+  if (error) {
+    console.log("Something went wrong", error);
+  } else {
+    console.log("Server is listening on port " + port);
+  }
+});
